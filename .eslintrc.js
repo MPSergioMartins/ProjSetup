7eadd6185da7f5fe499b529477253f6298452c71
MPSergioMartins <sergio.martins@mediaprobe.com>
2023-11-22 16:42:20 +0000

    update minor lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 /* eslint-env node */
 module.exports = {
-  env: { node: true },
+  env: { node: true, jest: true },
   extends: ["plugin:@typescript-eslint/recommended", "eslint:recommended"],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
